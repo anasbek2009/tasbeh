d20a5c000964254760de3b7b753ca3421cb6352e
John Doe <johndoe@example.com>
2022-09-13 20:06:06 +0500

    first JS

---
+++ app.js
@@ -0,0 +1,21 @@
+const lorem = document.querySelector(".count")
+        const box  = document.querySelector(".box")
+        let son = 0
+        function qoshish(){
+            son++
+        lorem.innerHTML = son
+
+        }
+        function nol(){
+            son = 0
+            lorem.innerHTML = son
+        }
+
+
+
+
+
+
+
+
+        
